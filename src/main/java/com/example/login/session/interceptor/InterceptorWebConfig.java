2a2c28b4064d1f21291ad74d4e462472f35c0dfe
Yang JiWoong <jerry3269@ajou.ac.kr>
2023-10-15 23:38:03 +0900

    #5 - Interceptor을 이용한 로그인

---
+++ src/main/java/com/example/login/session/interceptor/InterceptorWebConfig.java
@@ -0,0 +1,17 @@
+package com.example.login.session.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorWebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/interceptor/**")
+                .excludePathPatterns("/interceptor/sign-up", "/interceptor/login");
+    }
+}
